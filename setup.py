0a7c6b0add3f62f876b74f915aee6ad38acf8f4e
Casper Van Gheluwe <casper.vangheluwe@ugent.be>
2015-08-04 17:44:40 +0200

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(
-    name='pylex',
-    version='0.1.0',
+    name='lex4py',
+    version='1.0.0',
     package_dir={'': 'src'},
     packages=['pylex'],
     url='https://github.com/caspervg/pylex',
@@ -11,5 +11,20 @@ setup(
     author_email='caspervg@gmail.com',
     description='Python wrapper for the SC4Devotion LEX API',
     keywords='lex api wrapper sc4devotion sc4d csxlex bsclex',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Games/Entertainment :: Simulation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     install_requires=['requests>=2.7.0', 'nose>=1.3.7', 'requests-mock>=0.6.0']
 )
